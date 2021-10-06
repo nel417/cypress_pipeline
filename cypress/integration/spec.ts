4fd44462eabd6dd06e72fc007cd0172c5a891ed2
nel417 <n1ck@hey.com>
2021-10-06 16:57:34 -0500

    added a11y

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,7 +1,24 @@
-it('loads examples', () => {
-  cy.visit('/');
-  cy.get('h1').should('have.text', "Replace me with something relevant")
-  cy.url()
-  cy.get('button').click()
-  cy.get(".number-display").should('have.text', '1')
-});
+
+describe("Spec 1 tests", () => {
+  it('loads examples', () => {
+    cy.visit('/');
+    cy.get('h2').should('have.text', "hello")
+    cy.url()
+    cy.get('button').click()
+    cy.get(".number-display").should('have.text', '1')
+  });
+
+  describe("a11y", () => {
+    beforeEach(() => {
+      cy.injectAxe()
+    })
+    it("should have no accessibility errors!!!!", () => {
+      cy.checkA11y()
+    })
+  })
+})
+
+
+
+
+
